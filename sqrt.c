17a09c4ea5e26c4851b37164383c338720145954
mrityagi <mrityagi@gmail.com>
2020-10-13 17:03:03 +0530

    added DEBUG in printf statements

--- sqrt.c
+++ sqrt.c
@@ -36,12 +36,12 @@ int main(int argc, char* argv[]) {
 // argc being count of arguments
 // argv being value of arguments
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("DEBUG: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 	if(!is_actual_integer(argv[1]))
 	{
-		printf("Usage: Input should be a valid number\n\n");
+		printf("DEBUG: Usage: Input should be a valid number\n\n");
 		exit(-1);
 	}
 
@@ -49,11 +49,11 @@ int main(int argc, char* argv[]) {
 	if(argv[1][0]=='-')
 	{
 		input=-input;
-		printf("NEGSUPPORT:Sqrt of %d is %fi\n",input,sqrt(input));
+		printf("DEBUG:Sqrt of %d is %fi\n",input,sqrt(input));
 	}
 	else
-		printf("NEGSUPPORT:Sqrt of %d = %f\n",input,sqrt(input));
-	printf("NEGSUPPORT:End of program. Exiting now.");
+		printf("DEBUG:Sqrt of %d = %f\n",input,sqrt(input));
+	printf("DEBUG:End of program. Exiting now.");
 	return(0);
 
 } // end main
