455f04008db46c9e1349650ecd342b9ce8121698
mrityagi <mrityagi@gmail.com>
2020-10-13 16:50:37 +0530

    added NEGSUPPORT: to every printf statement in br_negativeSupport

--- sqrt.c
+++ sqrt.c
@@ -49,12 +49,11 @@ int main(int argc, char* argv[]) {
 	if(argv[1][0]=='-')
 	{
 		input=-input;
-		printf("Sqrt of %d is %fi\n",input,sqrt(input));
+		printf("NEGSUPPORT:Sqrt of %d is %fi\n",input,sqrt(input));
 	}
 	else
-		printf("Sqrt of %d = %f\n",input,sqrt(input));
-	printf("End of program. Exiting now.");
-	printf("Creating conflic in br_negativeSupport");
+		printf("NEGSUPPORT:Sqrt of %d = %f\n",input,sqrt(input));
+	printf("NEGSUPPORT:End of program. Exiting now.");
 	return(0);
 
 } // end main
